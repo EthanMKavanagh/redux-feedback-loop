1a7fd573bf737bc533b46d198330a619e5823284
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-25 18:09:17 -0500

    downloaded dependencies and set up index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feedbackForm = (state=[], action) => {
+    return state;
+}
+
+const reduxStore = createStore(
+    combineReducers({
+        feedbackForm: feedbackForm
+    }),
+    applyMiddleware(logger)
+)
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
