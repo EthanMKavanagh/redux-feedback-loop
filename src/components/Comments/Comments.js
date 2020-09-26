9031e7af67f11e8bb0e5f4bba5513b81e51aaaab
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-26 10:51:46 -0500

    added Comments

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 class Comments extends Component {
+
+    onNext = () => {
+        this.props.history.push('/review');
+    }
+
+    onChangeComments = (event) => {
+        this.props.dispatch({
+            type: 'SET_COMMENTS',
+            payload: event.target.value
+        });
+    }
+
     render() {
         return (
             <div>
-
+                <h1>Any comments you want to leave?</h1>
+                <h5>Comments?</h5>
+                <input
+                    type='text'
+                    onChange={this.onChangeComments}
+                />
+                <button onClick={this.onNext}>Next</button>
             </div>
         );
     }
