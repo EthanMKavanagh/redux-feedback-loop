cd0a92ed7f35e7822daaa18d450ffe6a718dbad8
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-26 11:29:48 -0500

    added server side POST

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -4,7 +4,18 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 router.post('/', (req, res) => {
-    console.log('server POST!');
+    console.log('In server POST');
+    let newFeedback = req.body;
+    let queryString = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+                       VALUES ($1, $2, $3, $4);`;
+    pool.query(queryString,
+        [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments])
+        .then(result => {
+            res.sendStatus(201);
+    }).catch(err => {
+        console.error('Error in server POST:', err);
+        res.sendStatus(500);
+    });
 });
 
 module.exports = router;
