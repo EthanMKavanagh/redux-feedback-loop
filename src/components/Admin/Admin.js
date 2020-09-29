27351d1b09c2ab61a5d56fa8197cd7b4f00eef3d
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-28 19:00:57 -0500

    additional material-ui table

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Button, Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody} from '@material-ui/core';
+import '../components.css';
 
 class Admin extends Component {
     render() {
@@ -10,23 +11,43 @@ class Admin extends Component {
                     <Table aria-label="simple table">
                         <TableHead>
                             <TableRow>
-                                <TableCell>Feeling</TableCell>
-                                <TableCell>Comprehension</TableCell>
-                                <TableCell>Support</TableCell>
-                                <TableCell>Comments</TableCell>
-                                <TableCell>Flag</TableCell>
-                                <TableCell>Delete</TableCell>
+                                <TableCell align='center'>Feeling</TableCell>
+                                <TableCell align='center'>Comprehension</TableCell>
+                                <TableCell align='center'>Support</TableCell>
+                                <TableCell align='left'>Comments</TableCell>
+                                <TableCell align='center'>Flag</TableCell>
+                                <TableCell align='center'>Delete</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
                             {this.props.feedback.map(item =>
                                 <TableRow component='th' scope='row'>
-                                    <TableCell align='left'>{item.feeling}</TableCell>
-                                    <TableCell align='left'>{item.understanding}</TableCell>
-                                    <TableCell align='left'>{item.support}</TableCell>
-                                    <TableCell align='left'>{item.comments}</TableCell>
-                                    <TableCell align='left'>Flagged?</TableCell>
-                                    <TableCell align='left'>
+                                    <TableCell 
+                                        align='center'
+                                        className='numberCell'>
+                                        {item.feeling}
+                                    </TableCell>
+                                    <TableCell 
+                                        align='center'
+                                        className='numberCell'>
+                                        {item.understanding}
+                                    </TableCell>
+                                    <TableCell 
+                                        align='center'
+                                        className='numberCell'>
+                                        {item.support}
+                                    </TableCell>
+                                    <TableCell 
+                                        align='left'
+                                        className='commentsCell'>
+                                        {item.comments}
+                                    </TableCell>
+                                    <TableCell  
+                                        align='center'>
+                                        Flagged?
+                                    </TableCell>
+                                    <TableCell 
+                                        align='center'>
                                         <Button 
                                             color='secondary' 
                                             variant='contained'>
